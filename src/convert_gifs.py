a67ec56065dc1d46e526d6d4cf5e214348e767e8
t-ube <ube@coder.okinawa.jp>
2026-02-24 00:56:53 +0900

    update

--- src/convert_gifs.py
+++ src/convert_gifs.py
@@ -4,6 +4,9 @@ import subprocess
 import os
 import boto3
 from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def ipfs_to_http(url):
     if url.startswith('ipfs://'):
@@ -12,12 +15,20 @@ def ipfs_to_http(url):
         return 'https://arweave.net/' + url[5:]
     return url
 
-supabase = create_client(os.environ['SUPABASE_URL'], os.environ['SUPABASE_KEY'])
+# 環境変数
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+R2_ENDPOINT = os.environ.get("R2_ENDPOINT")
+R2_ACCESS_KEY = os.environ.get("R2_ACCESS_KEY")
+R2_SECRET_KEY = os.environ.get("R2_SECRET_KEY")
+R2_BUCKET = os.environ.get("R2_BUCKET")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 s3 = boto3.client('s3',
-    endpoint_url=os.environ['R2_ENDPOINT'],
-    aws_access_key_id=os.environ['R2_ACCESS_KEY'],
-    aws_secret_access_key=os.environ['R2_SECRET_KEY'],
+    endpoint_url=R2_ENDPOINT,
+    aws_access_key_id=R2_ACCESS_KEY,
+    aws_secret_access_key=R2_SECRET_KEY,
 )
 
 res = supabase.table('uri_cache') \
@@ -49,7 +60,7 @@ for item in res.data or []:
 
         # R2アップロード
         mp4_key = item['uri'] + '.mp4'
-        s3.upload_file(output_path, os.environ['R2_BUCKET'], mp4_key,
+        s3.upload_file(output_path, R2_BUCKET, mp4_key,
                        ExtraArgs={'ContentType': 'video/mp4'})
 
         # DB更新
